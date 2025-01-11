4b9008330280b36e3a0bf5d6e2e6179467f76135
troywein <troywein@gmail.com>
2025-01-11 10:30:22 -0700

    s25

--- listAsLinkedListUnitTests.c
+++ listAsLinkedListUnitTests.c
@@ -9,9 +9,9 @@
 
 int main() {
 
-    // Insert an appropriate test harness for you linked list implementation.  
+    // Insert an appropriate test harness for your linked list implementation.  
     // Your tests will test each function when the list is empty, has a single element, and will also
-    // test modifications to the head, tail, or middle elements.
+    // test modifications to the head, tail, and middle elements.
 
     // You are not being judged/graded on the quality of your lab
     // However, it is in your best interest to make a high quality ADT for future assignments . . . hint hint
